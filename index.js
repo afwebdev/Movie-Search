e5cd505fa9b64d884856905dede86dba35fd0ff9
AFWebDev <andrewfariaj@gmail.com>
2019-03-30 19:00:27 -0400

    added viewport

--- index.js
+++ index.js
@@ -85,21 +85,12 @@ $(document).ready(function () {
   });
 
 
-
   $(document).on('click', '.overlay', function (e) {
     var sendTitle = $(this).text();
     $("#movieList li").fadeOut(1000);
     $('.movie').fadeIn(3500);
-    $('.wrapper').css("height", "100%");
     console.log(sendTitle);
     getFullInfo(sendTitle);
   });
-
-
-
-
-
-
-
 
 });
